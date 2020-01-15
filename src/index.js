03c543eb40116c329b250db4c703819eed9ef282
Giuseppe Setem <giu7d@github.com>
2020-01-15 10:33:21 -0300

    First styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./global.scss";
+import "./index.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="form-fild">
+            <label htmlFor="github_username">Usuário do GitHub:</label>
+            <input name="github_username" id="github_username" required></input>
+          </div>
+
+          <div className="form-fild">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required></input>
+          </div>
+
+          <div className="form-group">
+            <div className="form-fild">
+              <label htmlFor="lat">Latitude</label>
+              <input name="lat" id="lat" required></input>
+            </div>
+            <div className="form-fild">
+              <label htmlFor="lon">Longitude</label>
+              <input name="lon" id="lon" required></input>
+            </div>
+          </div>
+
+          <button type="submit">CRIAR</button>
+        </form>
+      </aside>
+      <main>main</main>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
